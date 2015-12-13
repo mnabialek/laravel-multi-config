24b93886c7a9d21af831ef498f519fcd7ab35e6d
mnabialek <gitre@nabialek.org>
2015-12-13 15:42:42 +0100

    Initial commit - created Service Provider and baisc module description

---
+++ readme.md
@@ -0,0 +1,9 @@
+Laravel Multi Config
+======
+## Purpose
+
+The purpose of this module is:
+
+- allow to use multiple Configuration folders (as in Laravel 4)
+- allow to use .env file specific for chosen environment
+- allow to set current environment based on current HTTP_HOST
---
+++ src/ServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mnabialek\LaravelMultiConfig;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
